7d6fd0dc4c9824b5ace49afc1f7d4b2eada7dd03
LexBondarchyk <108199972+LexBondarchyk@users.noreply.github.com>
2022-11-27 16:48:01 +0200

    1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -7,18 +7,18 @@ const refs = {
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
-// const colors = [
-//   "#FFFFFF",
-//   "#2196F3",
-//   "#4CAF50",
-//   "#FF9800",
-//   "#009688",
-//   "#795548",
-// ];
+const colors = [
+  "#FFFFFF",
+  "#2196F3",
+  "#4CAF50",
+  "#FF9800",
+  "#009688",
+  "#795548",
+];
 
-// const randomIntegerFromInterval = (min, max) => {
-//   return Math.floor(Math.random() * (max - min + 1) + min);
-// };
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
 
 const timer = {
   isActive: false,
